b18408521d5ce8c42783f7b059aa499f223fecba
Dany <danycambrano1@gmail.com>
2023-10-26 14:10:30 -0600

    reduce

---
+++ ejemplo2.js
@@ -0,0 +1,19 @@
+//concantenando arreglos
+arreglo1=[3,4,5,6,7];
+arreglo2=[8,9,12,11];
+
+/*function concantenr(){
+    //const result=arreglo1+","+arreglo2;
+    arreglo1=arreglo1.concat(arreglo2)
+    console.log(arreglo1);
+}
+//console.log(concantenr());*/
+
+//operaciones valores con reduce
+
+let suma = arreglo1.reduce((a,b)=>{return a+b})
+console.log(suma);
+
+
+
+
